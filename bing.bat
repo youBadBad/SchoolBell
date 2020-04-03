a86b2e1c5e4edc3e3394cd041502e7ba0b20a64c
Xin <wiserxin@163.com>
2020-04-03 19:31:33 +0800

    Create bing.bat

---
+++ bing.bat
@@ -0,0 +1,6 @@
+@ Rem this is a note/comment
+:: also a note/comment
+:: grammer ref [https://blog.csdn.net/qq_36838191/article/details/83046599]
+
+@ echo ***** welcome use wiserxin's school bell *****
+@ python bing.py
